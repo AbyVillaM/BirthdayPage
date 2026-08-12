bd942cc0108729ba2e23b68e03fc9b5ac64a55c1
ABIGAIL VILLA MILLAN <78711138+AbyVillaM@users.noreply.github.com>
2026-08-11 18:00:32 -0600

    Add files via upload
    last changes

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 function openGift() {
+
     const wrapper = document.getElementById("gift-wrapper");
     const card = document.getElementById("greeting-card");
 
     wrapper.classList.add("opened");
 
-    setTimeout(() => {
+    setTimeout(function () {
         card.classList.add("show");
     }, 900);
 }
 
+
+function openLetter() {
+
+    window.location.href = "carta.html";
+
+}
